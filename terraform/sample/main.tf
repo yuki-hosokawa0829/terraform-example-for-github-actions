72833981db2bdc7725bf79b610776b46614d260d
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-16 23:59:10 +0900

    add subnet

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -35,4 +35,11 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.0.0/24"]
 }
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["192.168.1.0/24"]
+}
   
