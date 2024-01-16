89291247b9d6531257af2df6d8eb91b4112fa444
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-16 23:55:44 +0900

    modify main file

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -15,23 +15,23 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 #Create Resource Group
-resource "azurerm_resource_group" "tamops" {
-  name     = "github-thomasthorntoncloud-terraform-example"
-  location = "uksouth"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-example"
+  location = "japanwest"
 }
 
 #Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "tamops-vnet"
   address_space       = ["192.168.0.0/16"]
-  location            = "uksouth"
-  resource_group_name = azurerm_resource_group.tamops.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.tamops.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.0.0/24"]
 }
