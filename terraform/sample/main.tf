f76e7ce60c4f4737f59889e0e63e08703fa595cc
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-16 23:58:10 +0900

    modify vnet name

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "rg" {
 
 #Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "tamops-vnet"
+  name                = "vnet"
   address_space       = ["192.168.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
