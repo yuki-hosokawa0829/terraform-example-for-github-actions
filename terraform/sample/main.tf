51cee03dd5a404cc36591c2301e1ebbe167d8ed3
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-17 18:17:21 +0900

    modify main file

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -29,22 +29,22 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 # Create Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
+resource "azurerm_subnet" "subnet0" {
+  name                 = "subnet0"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.0.0/24"]
 }
 
-resource "azurerm_subnet" "subnet2" {
-  name                 = "subnet2"
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.1.0/24"]
 }
 
-resource "azurern_subnet" "subnet3" {
-  name                 = "subnet3"
+resource "azurern_subnet" "subnet2" {
+  name                 = "subnet2"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.2.0/24"]
