8e1fae6697d66d270d1f4ccc86a738c9deb63e5b
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-18 01:15:31 +0900

    add network resource

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -36,3 +36,29 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.${count.index}.0/24"]
 }
+
+# Create NSG
+resource "azurerm_network_security_group" "nsg" {
+  count               = 3
+  name                = "nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  security_rule = {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "219.166.164.110"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
+  count                 = 3
+  subnet_id             = azurerm_subnet.subnet[count.index].id
+  network_security_group_id = azurerm_network_security_group.nsg[count.index].id
+}
