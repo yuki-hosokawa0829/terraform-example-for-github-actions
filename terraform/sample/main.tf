721373e6919c3a737d7a3802d99d761934f87ac2
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-18 00:47:49 +0900

    delete subnet2

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -42,10 +42,3 @@ resource "azurerm_subnet" "subnet1" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.1.0/24"]
 }
-
-resource "azurerm_subnet" "subnet2" {
-  name                 = "subnet2"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["192.168.2.0/24"]
-}
