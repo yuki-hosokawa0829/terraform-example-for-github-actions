eb8edd46b3c0ff3125d25795dcdeb658ba8f61be
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-18 01:20:46 +0900

    commit

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_subnet" "subnet" {
 # Create NSG
 resource "azurerm_network_security_group" "nsg" {
   count               = 3
-  name                = "nsg"
+  name                = "nsg${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   security_rule = {
