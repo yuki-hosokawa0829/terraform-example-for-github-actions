76f4149d4185c9a0dff61f2c6d53501ba86533fe
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-18 14:03:14 +0900

    format

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -44,16 +44,16 @@ resource "azurerm_network_security_group" "nsg" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   security_rule {
-    name                                       = "AllowRDP"
-    description                                = "Allow RDP"
-    priority                                   = 1000
-    direction                                  = "Inbound"
-    access                                     = "Allow"
-    protocol                                   = "Tcp"
-    source_port_range                          = "*"
-    destination_port_range                     = "3389"
-    source_address_prefix                      = "219.166.164.110"
-    destination_address_prefix                 = "*"
+    name                       = "AllowRDP"
+    description                = "Allow RDP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "219.166.164.110"
+    destination_address_prefix = "*"
   }
 }
 
