b4cbc0e9c77a2a174e8238441b65e5c60d33f3be
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-20 03:29:51 +0900

    commit tf file

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -55,18 +55,6 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "219.166.164.110"
     destination_address_prefix = "*"
   }
-  security_rule {
-    name                       = "AllowHTTP"
-    description                = "Allow HTTP"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
 # Associate NSG to Subnet
