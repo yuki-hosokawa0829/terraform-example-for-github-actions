0c41316e8107d8c9b39257f80b9d4503ccd5dca9
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-29 15:16:31 +0900

    Test Chekov

--- terraform/sample/main.tf
+++ terraform/sample/main.tf
@@ -20,23 +20,6 @@ resource "azurerm_resource_group" "rg" {
   location = "japanwest"
 }
 
-#Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet"
-  address_space       = ["192.168.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-# Create Subnet
-resource "azurerm_subnet" "subnet" {
-  count                = local.count
-  name                 = "subnet${count.index}"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["192.168.${count.index}.0/24"]
-}
-
 # Create NSG
 resource "azurerm_network_security_group" "nsg" {
   count               = local.count
@@ -62,6 +45,23 @@ resource "azurerm_subnet_network_security_group_association" "subnet_nsg_associa
   count                     = local.count
   subnet_id                 = azurerm_subnet.subnet[count.index].id
   network_security_group_id = azurerm_network_security_group.nsg[count.index].id
+}
+
+#Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  count                = local.count
+  name                 = "subnet${count.index}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["192.168.${count.index}.0/24"]
 }
 
 /* # Create Public IP
