9c6d9e25587d0e600a7f2a8260975b7313d1364c
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-29 18:10:55 +0900

    20220801

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -16,7 +16,7 @@ resource "azapi_resource" "ssh_public_key" {
 }
 
 resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  type        = "Microsoft.Compute/sshPublicKeys@2022-08-01"
   resource_id = azapi_resource.ssh_public_key.id
   action      = "generateKeyPair"
   method      = "POST"
