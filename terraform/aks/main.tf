b1f572376ca57a4c1698b0a16489056980d2ab74
yuki-hosokawa0829 <riversnow.onomatopeia@gmail.com>
2024-01-29 18:13:31 +0900

    modify api

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -9,7 +9,7 @@ resource "random_pet" "ssh_key_name" {
 }
 
 resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  type      = "Microsoft.Compute/sshPublicKeys@2022-08-01"
   name      = random_pet.ssh_key_name.id
   location  = var.resource_location
   parent_id = azurerm_resource_group.rg.id
