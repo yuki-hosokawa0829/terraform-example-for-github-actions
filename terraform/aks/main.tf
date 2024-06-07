8e209653eab43b02549a90044559c3f8f56fc344
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-07 23:05:49 +0900

    set sensitive true

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -102,6 +102,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "kube_config" {
