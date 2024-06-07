195d60af88763349bf5a8517e74d910807ee787a
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-07 11:17:26 +0900

    add backend setting

--- terraform/aks/provider.tf
+++ terraform/aks/provider.tf
@@ -15,10 +15,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "tamopstfstates"
-    storage_account_name = "tamopstf856fw017rbsal"
-    container_name       = "tfstatedevops"
-    key                  = "aks.main.tfstate"
+    resource_group_name  = var.backend_resource_group_name
+    storage_account_name = var.backend_storage_account_name
+    container_name       = var.backend_container_name
+    key                  = var.backend_key
   }
 }
 
--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -13,6 +13,22 @@ variable "aks_cluster_name" {
   default = "ndsou-test-tfaks01-cl"
 }
 
+variable "backend_resource_group_name" {
+  type    = string
+}
+
+variable "backend_storage_account_name" {
+  type    = string
+}
+
+variable "backend_container_name" {
+  type    = string
+}
+
+variable "backend_key" {
+  type    = string
+}
+
 locals {
   username = "ndsouadmin"
 }
