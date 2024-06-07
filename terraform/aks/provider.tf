6fee50fc071d6ea85e97ad539805c2d475e0f4af
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-07 12:24:05 +0900

    modify backend setting

--- terraform/aks/provider.tf
+++ terraform/aks/provider.tf
@@ -14,12 +14,8 @@ terraform {
       source  = "hashicorp/random"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = var.backend_resource_group_name
-    storage_account_name = var.backend_storage_account_name
-    container_name       = var.backend_container_name
-    key                  = var.backend_key
-  }
+
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
