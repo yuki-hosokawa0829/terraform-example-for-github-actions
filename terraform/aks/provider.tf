067876eeaf6f6ead8c435c652942ac8b9d0858b5
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-07 17:01:38 +0900

    fix azurerm version

--- terraform/aks/provider.tf
+++ terraform/aks/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=v1.1.4"
   required_providers {
     azurerm = {
-      version = "~>2.36.0"
+      version = "3.16.0"
       source  = "hashicorp/azurerm"
     }
     azapi = {
