4f16164513edbb725363b0e8f6e632e93a2cc8cc
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-20 13:52:10 +0900

    Tcp

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_network_security_group" "nsg" {
     priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3389"
     source_address_prefix      = "180.144.150.179"
