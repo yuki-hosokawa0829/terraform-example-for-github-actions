3cc108bff885cd7aa1df359a34a0de883a021da1
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-06-21 09:56:48 +0900

    add public IP address

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3389"
-    source_address_prefix      = "180.144.150.179"
+    source_address_prefix      = "180.144.150.179, 106.146.118.212"
     destination_address_prefix = "*"
   }
 }
