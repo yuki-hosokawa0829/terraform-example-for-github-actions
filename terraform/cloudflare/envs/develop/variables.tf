4e251c1d68da7d6c0192cafd2d4ebba5b65ba698
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-07-01 16:39:39 +0900

    remove backend variables

--- terraform/cloudflare/envs/develop/variables.tf
+++ terraform/cloudflare/envs/develop/variables.tf
@@ -18,22 +18,22 @@ variable "cloudflare_account_id" {
   sensitive   = true
 }
 
-variable "backend_endpoint" {
-  description = "The endpoint for the backend"
-  type        = string
-}
+#variable "backend_endpoint" {
+#  description = "The endpoint for the backend"
+#  type        = string
+#}
 
-variable "backend_bucket" {
-  description = "The bucket for the backend"
-  type        = string
-}
+#variable "backend_bucket" {
+#  description = "The bucket for the backend"
+#  type        = string
+#}
 
-variable "backend_key" {
-  description = "The key for the backend"
-  type        = string
-}
+#variable "backend_key" {
+#  description = "The key for the backend"
+#  type        = string
+#}
 
-variable "backend_region" {
-  description = "The region for the backend"
-  type        = string
-}
+#variable "backend_region" {
+#  description = "The region for the backend"
+#  type        = string
+#}
