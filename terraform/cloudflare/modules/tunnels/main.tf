fd0ab2d1c91af3ba18c2218df17e5f8398608b05
Yuki Hosokawa <P00018hosokawa-yuki@kec-miriz.com>
2024-07-08 14:32:46 +0900

    test

--- terraform/cloudflare/modules/tunnels/main.tf
+++ terraform/cloudflare/modules/tunnels/main.tf
@@ -48,7 +48,7 @@ resource "cloudflare_tunnel_config" "auto_tunnel" {
   config {
     ingress_rule {
       hostname = cloudflare_record.cname[count.index].hostname
-      service  = "http://web-service:80"
+      service  = "http://10.3.0.4:80"
     }
     ingress_rule {
       service = "http_status:404"
